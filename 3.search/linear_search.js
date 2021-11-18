0d0c291cfbad3b6e08fd66bc84c9cc0d56aefe52
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-11-18 23:07:55 +0900

    search

---
+++ 3.search/linear_search.js
@@ -0,0 +1,9 @@
+function linearSearch(arr, val){
+  for(var i = 0; i < arr.length; i++){
+    // console.log(arr[i])
+      if(arr[i] === val) return i;
+  }
+  return -1;
+}
+
+console.log(linearSearch([34,51,1,2,3,45,56,687], 2))
