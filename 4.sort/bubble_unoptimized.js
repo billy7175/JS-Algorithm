56794d2e1c6fc57e1d62a30dba2b85989605c1f2
billy7175 <68532934+billy7175@users.noreply.github.com>
2021-11-19 22:23:35 +0900

    bubble sort

---
+++ 4.sort/bubble_unoptimized.js
@@ -0,0 +1,16 @@
+// UNOPTIMIZED VERSION OF BUBBLE SORT
+function bubbleSort(arr){
+  for(var i = arr.length; i > 0; i--){
+    for(var j = 0; j < i - 1; j++){
+      console.log(arr);
+      if(arr[j] > arr[j+1]){
+        var temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;         
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([4,3,2,1]))
