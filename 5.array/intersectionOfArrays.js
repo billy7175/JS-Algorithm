97f1d5a471c9007feb460dad7aa1825a60f41414
billy7175 <whgus439@gmail.com>
2021-12-21 16:27:30 +0900

    intersectionOfTwoArrays

---
+++ 5.array/intersectionOfArrays.js
@@ -0,0 +1,4 @@
+var plants1 = ["Saturn","Earth","Uranus","Mercury","Venus","Earth","Mars","Jupiter"];
+var plants2 = ["Saturn","Earth","Uranus"];
+var alonePlants = [...new Set(plants1)].filter(item => plants2.includes(item));
+console.log(alonePlants); // [‘Saturn","Earth","Uranus"]
