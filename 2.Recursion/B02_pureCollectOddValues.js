607169f50ff738e444858aa951ba2b90a1c5f2ab
billy7175 <whgus439@gmail.com>
2021-12-22 00:43:55 +0900

    Pure collectOddValues

---
+++ 2.Recursion/B02_pureCollectOddValues.js
@@ -0,0 +1,15 @@
+function collectOddValues(array) {
+    let result = []
+    if (array.length === 0) return result
+    if (array[0] % 2 !== 0) {
+        result.push(array[0])
+    }
+
+
+    result = result.concat(collectOddValues(array.slice(1)))
+    return result
+
+}
+
+const result = collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+console.log('Result : ', result)
