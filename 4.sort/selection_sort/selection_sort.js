958e11ea11ec1527baeb52052e558f0332aa1d91
billy7175 <whgus439@gmail.com>
2021-12-28 01:12:46 +0900

    optimized selection sort

--- 4.sort/selection_sort/selection_sort.js
+++ 4.sort/selection_sort/selection_sort.js
@@ -1,17 +1,19 @@
-function selectionSort(arr){
-    for(var i=0; i<arr.length; i++){
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
         var lowest = i;
-        for(j=i+1; j<arr.length; j++){
-            if(arr[j] < arr[lowest])
-            lowest = j
+        for (j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest])
+                lowest = j
         }
 
-        const temp = arr[i]
-        arr[i] = arr[lowest]
-        arr[lowest] = temp;
+        if (i !== lowest) {
+            const temp = arr[i]
+            arr[i] = arr[lowest]
+            arr[lowest] = temp;
+        }
     }
-    
+
     return arr
 }
 
-console.log(selectionSort([34,22,10,19,17]))
+console.log(selectionSort([34, 22, 10, 19, 17]))
