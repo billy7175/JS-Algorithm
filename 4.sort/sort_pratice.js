062b83ba5f50f84f61c32eeb6a3d5ec302433194
billy7175 <whgus439@gmail.com>
2021-12-30 01:09:25 +0900

    sort pratice

--- 4.sort/sort_pratice.js
+++ 4.sort/sort_pratice.js
@@ -1,24 +1,46 @@
-// 1. Create Bubble Sort Function
-function bubbleSort (arr){
-    var noSwaps;
-    for(var i=arr.length; i>0; i--){
-        noSwaps = true;
-        for(var j=0; j<i-1; j++){
-            if(arr[j] > arr[j+1]){
+function bubbleSort(arr) {
+    for (var i = arr.length; i > 0; i--) {
+        for (var j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 const temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp 
-                noSwaps = false;
-                console.log(i, j)
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp;
             }
         }
-        if(noSwaps) break;
     }
     return arr;
 }
 
-console.log(bubbleSort([56,23,1,77,9]))
+console.log(bubbleSort([100, 23, 67, 50, 1]));
 
-// 2. Create Selection Sort Function
+function selectionSort(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var lowest = i;
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowest]) {
+                lowest = j;
+            }
 
-// 3. Insertion Sort Function
+            if (i !== lowest) {
+                const temp = arr[i];
+                arr[i] = arr[lowest];
+                arr[lowest] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort([100, 23, 67, 50, 1]))
+
+function insertionSort(arr){
+    for(var i=1; i<arr.length; i++){
+        var currentVal = arr[i]
+        for(var j= i-1; j>=0 && arr[j] > currentVal; j--){
+            arr[j +1] = arr[j];
+        }
+        arr[j + 1] = currentVal;
+    }
+    return arr;
+}
+console.log(insertionSort([23, 100, 67, 50, 1]))
