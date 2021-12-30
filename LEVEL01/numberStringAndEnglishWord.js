75b59feaf4ecb87a625ffc00ce219e9be454fc22
billy7175 <whgus439@gmail.com>
2021-12-30 23:38:17 +0900

    Level01 : 숫자 문자열과 영단어

---
+++ LEVEL01/numberStringAndEnglishWord.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i = 0; i < numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        console.log(i, arr)
+        console.log('여기 i :', i)
+        answer = arr.join(i);
+        console.log(i, answer)
+    }
+
+    return Number(answer);
+}
+
+
+console.log(solution("2three45sixseven"));
+console.log(['A','B','C','D','E'].join(1))
