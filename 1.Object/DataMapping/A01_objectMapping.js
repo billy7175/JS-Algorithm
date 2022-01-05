458563ffdb2569fa00128fe3f2f01708b50a6d64
billy7175 <whgus439@gmail.com>
2022-01-05 22:40:26 +0900

    Data Mapping : apply to model object

---
+++ 1.Object/DataMapping/A01_objectMapping.js
@@ -0,0 +1,30 @@
+const Mapping = {
+    'ID': 'id',
+    'LotSerial': 'rollNumber',
+    'User15': 'quantity',
+    'Footage': 'footage',
+    'LotType': 'rollType',
+    'status': 'rollStatus',
+    'UOM': 'uom',
+};
+
+const object = {
+    'ID': 'id',
+    'LotSerial': 'rollNumber',
+
+};
+
+function mappingFunction(object, mapping) {
+    const result = {};
+    for (const key in mapping) {
+        const newKey = mapping[key];
+        if (object.hasOwnProperty(key)) {
+            result[newKey] = object[key];
+        } else {
+            result[newKey] = null;
+        }
+    }
+    return result;
+}
+
+console.log(mappingFunction(object, Mapping))
